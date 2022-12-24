f73600f0384b0c0a6057640cf08ec85dca8ecd34
Vinicios Wentz <vinicioswentz@Vinicioss-MacBook-Pro.local>
2022-12-24 14:26:31 -0300

    feat: chapter 1 - Get to know LiveView finished

---
+++ lib/pento_web/views/wrong_live.ex
@@ -0,0 +1,67 @@
+defmodule PentoWeb.WrongLive do
+  use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
+
+  def mount(_params, _session, socket) do
+    {:ok,
+     assign(socket,
+       number: to_string(Enum.random(1..10)),
+       score: 0,
+       message: "Make a guess:"
+     )}
+  end
+
+  def handle_event("guess", %{"number" => number}, socket) do
+    new_random = to_string(Enum.random(1..10))
+
+    if number == socket.assigns.number do
+      message = "Your guess: #{number}. Congrats. Guess again."
+      score = socket.assigns.score + 1
+
+      {:noreply,
+       assign(socket,
+         number: new_random,
+         score: score,
+         message: message
+       )}
+    else
+      message = "Your guess: #{number}. Wrong. Guess again."
+      score = socket.assigns.score - 1
+
+      {:noreply,
+       assign(socket,
+         number: new_random,
+         score: score,
+         message: message
+       )}
+    end
+  end
+
+  def handle_event("restart", _params, socket) do
+    {:ok,
+     assign(socket,
+       number: to_string(Enum.random(1..10)),
+       score: 0,
+       message: "Make a guess:"
+     )}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Your score: <%= @score %></h1>
+    <h2>
+      <%= @message %>
+      <%= @number %>
+    </h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+        <a href="#" phx-click="guess" phx-value-number={n}>
+          <%= n %>
+        </a>
+      <% end %>
+    </h2>
+    <button type="submit" phx-click="restart">
+      Restart
+    </button>
+    """
+  end
+end
