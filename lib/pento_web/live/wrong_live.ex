31dfe0605400f0a249b9b6adc5483c875ff4d0a0
Vinicios Wentz <vinicios.wentz@icloud.com>
2024-02-28 22:19:47 -0300

    docker and event handling

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,7 +2,44 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    number = Enum.random(1..10)
+
+    {:ok, assign(socket, score: 0, number: number, message: "Make a guess:") |> IO.inspect()}
+  end
+
+  def handle_event("guess", %{"number" => guess}, socket) do
+    socket =
+      socket
+      |> check_guess(guess)
+      |> update_socket_with_score_and_message()
+
+    {:noreply, socket}
+  end
+
+  defp update_socket_with_score_and_message(%{assigns: %{has_guessed: {true, message}}} = socket) do
+    score = socket.assigns.score + 1
+
+    assign(socket, message: message, score: score)
+  end
+
+  defp update_socket_with_score_and_message(%{assigns: %{has_guessed: {false, message}}} = socket) do
+    score = socket.assigns.score - 1
+
+    assign(socket, message: message, score: score)
+  end
+
+  defp check_guess(%{assigns: %{number: number}} = socket, guess) do
+    has_guessed =
+      case guess == to_string(number) do
+        true -> {true, "Your guess: #{guess}. Correct."}
+        _ -> {false, "Your guess: #{guess}. Wrong. Guess again"}
+      end
+
+    assign(socket, has_guessed: has_guessed)
+  end
+
+  def time() do
+    DateTime.utc_now() |> to_string
   end
 
   def render(assigns) do
@@ -10,7 +47,7 @@ defmodule PentoWeb.WrongLive do
     <h1 class="mb-4 text-4xl font-extrabold">Your score: <%= @score %></h1>
 
     <h2>
-      <%= @message %>
+      <%= @message %> It's <%= time() %>
     </h2>
     <br />
     <h2>
@@ -24,6 +61,7 @@ defmodule PentoWeb.WrongLive do
           <%= n %>
         </.link>
       <% end %>
+      <.link patch={~p"/guess"}>Restart</.link>
     </h2>
     """
   end
